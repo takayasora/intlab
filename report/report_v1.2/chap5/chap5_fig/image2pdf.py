58e395abc54457143e8763ff44f90aae9e2137d9
高矢 空 <79049713+Tsora1216@users.noreply.github.com>
2024-02-08 21:07:35 +0900

    Update report v1.2

--- report/report_v1.2/chap5/chap5_fig/image2pdf.py
+++ report/report_v1.2/chap5/chap5_fig/image2pdf.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import os
-from reportlab.pdfgen import canvas
 
 def convert_images_to_pdf(input_folder_path, output_folder_path):
-    # 指定フォルダ内のすべてのjpg, png, gifファイルを探す
+    # サポートする画像ファイルの拡張子
+    supported_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tif', '.tiff', '.webp')
+    
     for root, dirs, files in os.walk(input_folder_path):
         for file in files:
-            if file.lower().endswith(('.jpg', '.png', '.gif')):
+            if file.lower().endswith(supported_extensions):
                 image_path = os.path.join(root, file)
                 pdf_file_name = os.path.splitext(file)[0] + '.pdf'
                 pdf_path = os.path.join(output_folder_path, pdf_file_name)
@@ -17,7 +18,7 @@ def convert_images_to_pdf(input_folder_path, output_folder_path):
                         # 画像を開いてPDFに変換
                         with Image.open(image_path) as img:
                             # アルファチャネル（透明度）を持つ画像を処理
-                            if img.mode == 'RGBA':
+                            if img.mode == 'RGBA' or img.mode == 'LA':
                                 img = img.convert('RGB')
                             img.save(pdf_path, "PDF", resolution=100.0)
                             print(f"Converted {image_path} to {pdf_path}")
